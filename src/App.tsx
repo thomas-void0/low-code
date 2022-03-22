7b13483a28b8585dc0c58a71f479e60b0f227eb9
newrankyangjingyu <yangjingyu@newrank.cn>
2022-03-22 17:42:18 +0800

    feat:  新增拖拽

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { DndProvider, useDrag } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { ItemTypes } from "./dndTypes";
+
+function Knight() {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.KNIGHT,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: "bold",
+        cursor: "move",
+      }}
+    >
+      ♘
+    </div>
+  );
+}
+
 function App() {
-  return <div>咯w</div>;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Knight />
+    </DndProvider>
+  );
 }
 
 export default App;
