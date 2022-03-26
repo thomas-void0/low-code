270e769dc1880ad00327f1462cbbe1df7579e157
thomas <yjy15680489038@163.com>
2022-03-26 13:45:22 +0800

    update: 更新项目依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,5 @@
-import { DndProvider, useDrag } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { ItemTypes } from "./dndTypes";
-
-function Knight() {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: ItemTypes.KNIGHT,
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
-
-  return (
-    <div
-      ref={drag}
-      style={{
-        opacity: isDragging ? 0.5 : 1,
-        fontSize: 25,
-        fontWeight: "bold",
-        cursor: "move",
-      }}
-    >
-      ♘
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <Knight />
-    </DndProvider>
-  );
+  return <div>app</div>;
 }
 
 export default App;
