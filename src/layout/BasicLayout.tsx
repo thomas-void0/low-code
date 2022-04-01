9dfdeabf278566579b781b6cec6d2f1d19469b52
newrankyangjingyu <yjy15680489038@163.com>
2022-04-01 14:41:00 +0800

    feat: 新增头部封装

---
+++ src/layout/BasicLayout.tsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+// // 渲染头部
+// const headerRender = (props, matchMenuKeys: string[]): React.ReactNode => {
+// 	if (props.headerRender === false || props.pure) {
+// 		return null
+// 	}
+
+// 	return <HeaderView />
+// }
