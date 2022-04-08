606f421cf08fd6175b756d6f8600065cd570e845
yangjingyu <yangjingyu@newrank.cn>
2022-04-08 11:30:11 +0800

    update: update error catch

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ConfigProvider } from 'antd'
 import zh_CN from 'antd/lib/locale/zh_CN'
 import ProEmpty from './components/ProEmpty'
 import PageLoading from './components/Loading'
+import ErrorBoundary from './components/ErrorBoundary'
 import { GlobalProvider, AccessProvider, CreateRoutes } from './pro'
 import routes from './routes'
 
@@ -13,24 +14,26 @@ import './assets/css/index.less'
 
 function App() {
 	return (
-		<ThemeProvider>
-			<ConfigProvider
-				locale={zh_CN}
-				input={{ autoComplete: 'off' }}
-				form={{ validateMessages: { required: '请完善${label}' } }}
-				renderEmpty={() => <ProEmpty size="large" />}
-			>
-				<BrowserRouter basename="/">
-					<Suspense fallback={<PageLoading />}>
-						<GlobalProvider routes={routes}>
-							<AccessProvider routes={routes}>
-								<CreateRoutes />
-							</AccessProvider>
-						</GlobalProvider>
-					</Suspense>
-				</BrowserRouter>
-			</ConfigProvider>
-		</ThemeProvider>
+		<ErrorBoundary>
+			<ThemeProvider>
+				<ConfigProvider
+					locale={zh_CN}
+					input={{ autoComplete: 'off' }}
+					form={{ validateMessages: { required: '请完善${label}' } }}
+					renderEmpty={() => <ProEmpty size="large" />}
+				>
+					<BrowserRouter basename="/">
+						<Suspense fallback={<PageLoading />}>
+							<GlobalProvider routes={routes}>
+								<AccessProvider routes={routes}>
+									<CreateRoutes />
+								</AccessProvider>
+							</GlobalProvider>
+						</Suspense>
+					</BrowserRouter>
+				</ConfigProvider>
+			</ThemeProvider>
+		</ErrorBoundary>
 	)
 }
 export default App
